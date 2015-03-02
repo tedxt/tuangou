69c836a2de524139e398af633cb27d0744ad0d9f
piglikeyoung@qq.com <piglikeyoung@piglikeyoungdeMacBook-Pro.local>
2015-03-02 13:41:09 +0800

    Initial Commit

---
+++ 团购/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  团购
+//
+//  Created by piglikeyoung on 15/3/2.
+//  Copyright (c) 2015年 jinheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 团购/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  团购
+//
+//  Created by piglikeyoung on 15/3/2.
+//  Copyright (c) 2015年 jinheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
